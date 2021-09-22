b6dfb258c7cfade372afb4bea6c18289e40de9db
David Andrés Herrera <biggiegun@gmail.com>
2021-09-21 22:45:30 -0500

    Creación de mapa para Geolocalización

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Mapa from './components/Mapa'
+import 'bootstrap/dist/css/bootstrap.css'
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Mapa />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
